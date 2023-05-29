abf45b4263ac514a8d16da77d6b9a27e35fd664f
Sabrinaporfilia <sabrinaprogram99@gmail.com>
2023-05-29 19:21:33 -0300

    atualização exercicios

--- src/matrizes/Matrizes2.java
+++ src/matrizes/Matrizes2.java
@@ -7,28 +7,22 @@ public class Matrizes2 {
 	public static void main(String[] args) {
 		Scanner leia = new Scanner(System.in);
 		float matrizNotas[][] = new float[10][4];
-		float soma = 0, media = 0;
+		float soma = 0.0f;
+		float media[] = new float[10];
 
 		for (int linha = 0; linha < 10; linha++) {
 			System.out.println("Digite a nota do " + (linha + 1) + "º aluno:");
-			for (int coluna = 0; coluna < 4; coluna++)
+			for (int coluna = 0; coluna < 4; coluna++) {
 				matrizNotas[linha][coluna] = leia.nextFloat();
-
-		}
-		System.out.println("\nAs notas foram foram: \n");
-
-		for (int linha = 0; linha < 10; linha++) {
-			for (int coluna = 0; coluna < 4; coluna++) {
-				System.out.println((linha + 1) + "º aluno " + matrizNotas[linha][coluna]);
-			}
-
-		}
-		for (int linha = 0; linha < 10; linha++)
-			for (int coluna = 0; coluna < 4; coluna++) {
 				soma += matrizNotas[linha][coluna];
-
 			}
-
+			media[linha] = soma / 4;
+			soma = 0.0f; //reinicia a soma
+		}
+		System.out.println("\nMédias dos alunos:");
+		for (int linha = 0; linha < 10; linha++) {
+			System.out.printf("Aluno %d: %.1f\n", (linha + 1), media[linha]);
+		}
 	}
 
 }
